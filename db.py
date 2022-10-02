b4dbfc08fe1910696fe63df3136cf621831e2e6e
uch1104 <foajoaquin@gmail.com>
2022-10-02 22:05:10 +0900

    データベース情報追加

--- db.py
+++ db.py
@@ -2,7 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@127.0.0.1:3306/fastapi_db"
+# データベース情報
+USER = 'user'
+PASSWORD = 'password'
+SERVER = '127.0.0.1'
+PORT = '3306'
+DB = 'fastapi_db'
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{USER}:{PASSWORD}@{SERVER}:{PORT}/{DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
